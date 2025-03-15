09ed455712af5b43d5bec6fd7d5531fb97add110
MariaClaraMAF <mariaclaramonteiro@usp.br>
2025-03-15 15:11:27 -0300

    FirstProgram/firstprogram

---
+++ FirstProgram/firstprogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+
+    cout << "Number 1" << endl;
+    cout << "Number 2" << endl;
+    cout << "Number 3" << endl;
+
+    return 0;
+}
