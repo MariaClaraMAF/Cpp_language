0796232d09ec445adbef54f6876ca138c3493f75
MariaClaraMAF <mariaclaramonteiro@usp.br>
2025-04-23 20:50:49 -0300

    semana 1

--- HackerRank/Semana1/7- Pointers/main.cpp
+++ HackerRank/Semana1/7- Pointers/main.cpp
@@ -1,11 +1,43 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-constexpr int get_value(){
-    return 3;
+
+void atualiza(int *a, int *b){
+    int aux1, aux2;
+    aux1 = *a;
+    aux2 = *b;
+    *a = aux1+aux2;
+    *b = abs(aux1-aux2);
+
 }
 
-int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+int main() {
+    int end1, end2;
+    int *a, *b;
+
+    a = &end1;
+    b = &end2;
+
+    scanf("%d %d", &end1, end2);
+    atualiza(a,b);
+    cout << &end1 << endl;
+    cout << &end2 << endl;
+    // /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // int *a{nullptr};
+    // int *b{nullptr};
+    
+    // // Acessando o valor do ponteiro
+    // cout << a << endl;
+    // cout << b << endl;
+    
+    // // Acessando o endereço do ponteiro
+    // cout << &a << endl;
+    // cout << &b << endl;
+    
+    
     return 0;
 }
